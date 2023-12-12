43968029624e1171f97d9ed794725f60d7cfa8e6
Hamim Ahmed <hamim.ahmed484@gmail.com>
2023-12-12 23:14:32 +0600

    module-36(1-4) added

---
+++ Javascript_Object/compare.js
@@ -0,0 +1,23 @@
+const first = { a: 5, b: 2, c: 5 };
+const second = { a: 5, c: 5, b: 2 };
+const firstString = JSON.stringify(first);
+const secondString = JSON.stringify(second);
+
+function compareObject(first, second) {
+    const firstKeys = Object.keys(first);
+    const secondKeys = Object.keys(second);
+    if (firstKeys.length === secondKeys.length) {
+        for (const key of firstKeys) {
+            if (first[key] !== second[key]) {
+                return false;
+
+            }
+        }
+    }
+    else {
+        return false;
+    }
+}
+
+const isSame = compareObject(first, second);
+console.log(isSame);
